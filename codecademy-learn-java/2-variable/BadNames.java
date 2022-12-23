0c537b42e508640222dd146fd5da27edb0f2660b
thinh <thinh.personal@gmail.com>
2022-12-23 22:31:36 +0700

    Learn static type and write first program rely on First Head Java book

---
+++ codecademy-learn-java/2-variable/BadNames.java
@@ -0,0 +1,11 @@
+public class BadNames {
+  public static void main(String[] args) {
+    String firstName = "Samira";
+    String blah = "Smith";
+    String dotcom = "samira@google.com";
+    int salaryExpectation = 1000000;
+    int yearOfBirth = 1955;
+
+    System.out.println("The program runs!");
+  }
+}
---
+++ codecademy-learn-java/2-variable/MyProfile.java
@@ -0,0 +1,15 @@
+public class MyProfile {
+  public static void main(String[] args) {
+    String name = "Robert";
+    int age = 22;
+    double desiredSalary = 9.5;
+    char gender = 'm';
+    boolean lookingForJob = true;
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(desiredSalary);
+    System.out.println(gender);
+    System.out.println(lookingForJob);
+  }
+}
