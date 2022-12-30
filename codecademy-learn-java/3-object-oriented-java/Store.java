282dd83c93f66f9b41fc58222cea46a6818293dc
thinh <thinh.personal@gmail.com>
2022-12-30 12:17:54 +0700

    Store

---
+++ codecademy-learn-java/3-object-oriented-java/Store.java
@@ -0,0 +1,22 @@
+public class Store {
+  // scope of Store class starts after curly brace
+
+  // new method: constructor!
+  public Store() {
+    System.out.println("I am inside the constructor method.");
+  }
+
+  public static void main(String[] args) {
+    //  scope of main() starts after curly brace
+    System.out.println("Start of the main method.");
+    //  program tasks
+
+    // create the instance below
+    Store lemonadeStand = new Store();
+
+    System.out.println(lemonadeStand);
+
+  }
+  // scope of main() ends after curly brace
+}
+// scope of Store class ends after curly brace
