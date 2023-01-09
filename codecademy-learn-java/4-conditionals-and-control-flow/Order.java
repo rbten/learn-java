40f8e3ff44012a68300b26ec0af327a791b48f07
thinh <thinh.personal@gmail.com>
2023-01-09 16:05:24 +0700

    Order

--- codecademy-learn-java/4-conditionals-and-control-flow/Order.java
+++ codecademy-learn-java/4-conditionals-and-control-flow/Order.java
@@ -1,16 +1,48 @@
 public class Order {
-  public static void main(String[] args) {
-    double itemCost = 30.99;
-    if (itemCost>24.00) {
-      System.out.println("High value item!");
+  boolean isFilled;
+  double billAmount;
+  String shipping;
+  String couponCode;
+
+  public Order (boolean filled, double cost, String shippingMethod, String coupon) {
+    if (cost > 24.00) {
+      System.out.println("Hight value item!");
     }
 
-    boolean isFilled = false;
+    isFilled = filled;
+    billAmount = cost;
+    shipping = shippingMethod;
+    couponCode = coupon;
+  }
 
+  public void ship() {
     if (isFilled) {
       System.out.println("Shipping");
+      System.out.println("Shipping cost: "+calculateShipping());
     } else {
       System.out.println("Order not ready");
     }
+  }
+
+  public double calculateShipping() {
+    if (shipping.equals("Regular")) {
+      return 0;
+    } else if (shipping.equals("Express")) {
+      if (couponCode.equals("ship50")) {
+        return 0.75;
+      } else {
+        return 1.75;
+      }
+    } else {
+      return 0.5;
+    }
+  }
+
+  public static void main(String[] args) {
+    Order book = new Order(true, 9.99, "Express", "ship50");
+    Order chemistrySet = new Order(true, 72.50, "Regular", "freeship");
+
+    book.ship();
+    chemistrySet.ship();
   }
 }
