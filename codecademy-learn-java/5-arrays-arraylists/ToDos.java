4902331cde4cbdef6268877491d6b82ff7fd0fc0
thinh <thinh.personal@gmail.com>
2023-01-15 22:34:46 +0700

    ToDos

--- codecademy-learn-java/5-arrays-arraylists/ToDos.java
+++ codecademy-learn-java/5-arrays-arraylists/ToDos.java
@@ -17,5 +17,33 @@ public class ToDos {
 
     String[] test = {"Ro", "10PM", "now"};
     System.out.println(Arrays.toString(test));
+
+    // Sherlock todo
+    ArrayList<String> sherlocksToDos = new ArrayList<String>();
+
+    sherlocksToDos.add("visit the crime scene");
+    sherlocksToDos.add("play violin");
+    sherlocksToDos.add("interview suspects");
+    sherlocksToDos.add("solve the case");
+    sherlocksToDos.add("apprehend the criminal");
+
+    // Poirot
+    ArrayList<String> poirotToDos = new ArrayList<String>();
+
+    poirotToDos.add("visit the crime scene");
+    poirotToDos.add("interview suspects");
+    poirotToDos.add("let the little grey cells do their work");
+    poirotToDos.add("trim mustache");
+    poirotToDos.add("call all suspects together");
+    poirotToDos.add("reveal the truth of the crime");
+
+    System.out.println(sherlocksToDos.size());
+    System.out.println(poirotToDos.size());
+
+    if (sherlocksToDos.size() > poirotToDos.size()) {
+      System.out.println("sherlocksToDos");
+    } else {
+      System.out.println("poirotsToDos");
+    }
   }
 }
