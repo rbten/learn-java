cd148367bf6f88ded5b0c5219820709c4b19062a
thinh <thinh.personal@gmail.com>
2023-01-18 12:10:29 +0700

    Numbers, CheckItems

---
+++ codecademy-learn-java/6-loops/CheckItems.java
@@ -0,0 +1,18 @@
+public class CheckItems {
+  public static boolean checkForJacket(String[] lst) {
+    for (int i = 0; i < lst.length; i++) {
+      System.out.println(lst[i]);
+      if (lst[i].equals("jacket")) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    String[] suitcase = {"shirt", "jacket", "pants", "socks"};
+
+    System.out.println(checkForJacket(suitcase));
+  }
+}
