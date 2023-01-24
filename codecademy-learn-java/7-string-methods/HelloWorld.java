3a3cadfd47af883efc49290e16aeea90895b746b
thinh <thinh.personal@gmail.com>
2023-01-24 13:11:00 +0700

    HelloWorld

---
+++ codecademy-learn-java/7-string-methods/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    String aGreet = "Hello, World!";
+
+    System.out.println(aGreet.length());
+    System.out.println(aGreet.substring(4));
+    System.out.println(aGreet.toUpperCase());
+  }
+}
