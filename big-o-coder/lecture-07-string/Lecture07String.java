4e816d06ff4881ce93e67bd08f32098cd3c97675
thinh <thinh.personal@gmail.com>
2023-02-23 11:33:27 +0700

    Lecture07String

---
+++ big-o-coder/lecture-07-string/Lecture07String.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Lecture07String{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // Kieu ky tu
+
+    // char c = sc.nextLine().charAt(0);
+    // System.out.println(c);
+    // c = '!';
+    // System.out.println(c);
+
+    // Kieu chuoi
+
+    // String myName = "";
+    // myName = sc.nextLine();
+    // System.out.println("Ban vua nhap: " + myName);
+    // System.out.println("Do dai: " + myName.length());
+
+    // Gan bang 2 chuoi
+    // String name1 = "Robert";
+    // String name2 = name1 + "22222";
+
+    // System.out.println("Name 1: " + name1);
+    // System.out.println("Name 2: " + name2);
+    
+    // Truy Xuat 
+    // String myName = sc.nextLine();
+    // System.out.println("Ban vua nhap: " + myName);
+    // System.out.println("Do dai: " + myName.length());
+    // System.out.println(myName.charAt(-1));
+
+    char kyTu = '3';
+    System.out.println(kyTu);
+  }
+}
