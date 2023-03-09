af3da6126a7fac4e90e44c77a902e164680030d8
thinh <thinh.personal@gmail.com>
2023-03-09 22:38:43 +0700

    static-methods-of-the-math-class

---
+++ codecademy-learn-java/8-access-encapsulation-static-methods/2-static-methods-of-the-math-class/OptionTwoStaticMethods.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.*;
+
+public class OptionTwoStaticMethods{
+  public static void main(String[] args) {
+    int smallerNumber = min(3, 10);
+    System.out.println(smallerNumber);
+  }
+}
