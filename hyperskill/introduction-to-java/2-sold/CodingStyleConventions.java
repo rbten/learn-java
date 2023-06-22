56eeb98ae4d42e9a3644fba28a9191eda2c067f8
thinh <thinh.personal@gmail.com>
2023-06-22 11:36:56 +0700

    CodingStyleConventions, ScanningTheInput

---
+++ hyperskill/introduction-to-java/2-sold/CodingStyleConventions.java
@@ -0,0 +1,7 @@
+public class CodingStyleConventions {
+
+    public static void main(String[] args){
+        System.out.println("Hi!");
+        System.out.println("I'm a Java program.");
+    }
+}
--- hyperskill/introduction-to-java/2-sold/ScanningTheInput.java
+++ hyperskill/introduction-to-java/2-sold/ScanningTheInput.java
@@ -5,6 +5,17 @@ public class ScanningTheInput {
     Scanner scanner = new Scanner(System.in);
     String myName = scanner.next();
 
-    System.out.println("Hello, " + myName + "!");
+    String word1 = scanner.next();
+    String line1 = scanner.nextLine();
+    String word2 = scanner.next();
+    String word3 = scanner.next();
+    String line2 = scanner.nextLine();
+
+    System.out.println("word1: " + word1);
+    System.out.println("line1: " + line1);
+    System.out.println("word2: " + word2);
+    System.out.println("word3: " + word3);
+    System.out.println("line2: " + line2);
+
   }
 }
