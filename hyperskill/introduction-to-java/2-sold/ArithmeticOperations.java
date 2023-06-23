afdba4b96957a5b371eec671990320378f5be0ef
thinh <thinh.personal@gmail.com>
2023-06-23 16:48:00 +0700

    ArithmeticOperations

---
+++ hyperskill/introduction-to-java/2-sold/ArithmeticOperations.java
@@ -0,0 +1,27 @@
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        System.out.println(13 + 25);
+        System.out.println(20 + 70);
+
+        System.out.println(70 - 30);
+        System.out.println(30 - 70);
+
+        System.out.println(21 * 3);
+        System.out.println(20 * 10);
+
+        System.out.println(8 / 3);
+        System.out.println(41 / 5);
+
+        System.out.println(10 % 3);
+        System.out.println(12 % 4);
+        System.out.println(5 % 9);
+
+        System.out.println(1 + 3 * 4 - 2);
+        System.out.println((1 + 3) * (4 - 2));
+
+        System.out.println(+5);
+        System.out.println(-8);
+        System.out.println(-(100 + 4));
+    }
+}
