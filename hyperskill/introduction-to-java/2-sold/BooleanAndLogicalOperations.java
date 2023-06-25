3d89eb9cc35192c6290d2f2b3cc37590fae1688f
thinh <thinh.personal@gmail.com>
2023-06-25 20:58:22 +0700

    BooleanAndLogicalOperations

---
+++ hyperskill/introduction-to-java/2-sold/BooleanAndLogicalOperations.java
@@ -0,0 +1,10 @@
+public class BooleanAndLogicalOperations {
+    
+    public static void main(String[] args) {
+        boolean open = true;
+        boolean closed = false;
+
+        System.out.println(open);   // true
+        System.out.println(closed); // false
+    }
+}
