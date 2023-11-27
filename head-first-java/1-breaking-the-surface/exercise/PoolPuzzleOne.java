70c6e1245c933dc86178b9e54a9b38a3c0ef7973
thinh <thinh.personal@gmail.com>
2023-11-27 21:52:25 +0700

    exercise folder

---
+++ head-first-java/1-breaking-the-surface/exercise/PoolPuzzleOne.java
@@ -0,0 +1,30 @@
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 4) {
+            System.out.print("an");
+            if (x < 1) {
+                ------0-
+            }
+            ------
+            if (----) {
+                -----
+                -----
+            }
+            if (x == 1) {
+                -----
+            }
+            if (----) {
+                --------
+            }
+            System.out.println();
+            -------
+        }
+    }
+}
+
+/*
+a noise
+annoys
+an oyster
+*/
