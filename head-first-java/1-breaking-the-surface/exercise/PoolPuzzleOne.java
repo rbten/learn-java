dfcfed729f68b97d3088450e0d694bccb8cdcdde
thinh <thinh.personal@gmail.com>
2023-11-29 20:54:02 +0700

    PoolPuzzleOne

--- head-first-java/1-breaking-the-surface/exercise/PoolPuzzleOne.java
+++ head-first-java/1-breaking-the-surface/exercise/PoolPuzzleOne.java
@@ -2,23 +2,26 @@ public class PoolPuzzleOne {
     public static void main(String[] args) {
         int x = 0;
         while (x < 4) {
-            System.out.print("an");
+            System.out.print("a");
+
             if (x < 1) {
-                ------0-
+                System.out.print(" ");
             }
-            ------
-            if (----) {
-                -----
-                -----
+
+            System.out.print("n");
+
+            if (x < 1) {
+                System.out.print("oise");
+                x = x - 1;
             }
             if (x == 1) {
-                -----
+                System.out.print("noys");
             }
-            if (----) {
-                --------
+            if (x > 1) {
+                System.out.print(" oyster");
             }
             System.out.println();
-            -------
+            x = x + 2;
         }
     }
 }
