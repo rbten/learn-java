d5dc71f07848a692ea890bd19e1583f4f597b341
thinh <thinh.personal@gmail.com>
2023-12-13 21:26:54 +0700

    PrimitiveVariable

---
+++ head-first-java/3/PrimitiveVariable.java
@@ -0,0 +1,16 @@
+public class PrimitiveVariable {
+    public static void main(String[] args) {
+        int x = 12;
+        char initial = 'j';
+        double d = 456.709;
+        boolean isLearning;
+        isLearning = true;
+        int y = x + 456;
+
+        System.out.println(x);
+        System.out.println(initial);
+        System.out.println(d);
+        System.out.println(isLearning);
+        System.out.println(y);
+    }
+}
