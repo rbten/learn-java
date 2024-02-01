7731f755b7847174227ae945acfb713b716b7232
thinh <thinh.personal@gmail.com>
2024-02-01 21:36:00 +0700

    Add Lecture 01

---
+++ big-o-coder/lecture-01-introduction/lesson/Lesson01.java
@@ -0,0 +1,10 @@
+public class Lesson01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println(3);
+        System.out.println(3.14);
+        System.out.println(4E3);
+
+        
+    }
+}
