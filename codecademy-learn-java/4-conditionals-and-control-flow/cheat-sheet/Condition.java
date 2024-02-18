fa5d526fde5b181abf619510fd9ae92790fa233e
thinh <thinh.personal@gmail.com>
2024-02-18 23:12:11 +0700

    Add cheat sheet to 4-conditionals-and-control-flow

---
+++ codecademy-learn-java/4-conditionals-and-control-flow/cheat-sheet/Condition.java
@@ -0,0 +1,13 @@
+public class Condition {
+    public static void main(String[] args) {
+        boolean condition1 = false;
+
+    if (condition1){
+        System.out.println("condition1 is true");
+    }
+    else{
+        System.out.println("condition1 is not true");
+    }
+    // Prints: condition1 is not true
+    }
+}
