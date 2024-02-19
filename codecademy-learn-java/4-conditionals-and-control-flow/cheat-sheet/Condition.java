beebc6b17c923d12ce64a07ca8a0510380e7ca6a
thinh <thinh.personal@gmail.com>
2024-02-19 11:01:17 +0700

    update cheat-sheet in 4-conditionals-and-control-flow

--- codecademy-learn-java/4-conditionals-and-control-flow/cheat-sheet/Condition.java
+++ codecademy-learn-java/4-conditionals-and-control-flow/cheat-sheet/Condition.java
@@ -1,13 +1,68 @@
 public class Condition {
     public static void main(String[] args) {
-        boolean condition1 = false;
+        // else Statement
+        // boolean condition1 = false;
 
-    if (condition1){
-        System.out.println("condition1 is true");
-    }
-    else{
-        System.out.println("condition1 is not true");
-    }
-    // Prints: condition1 is not true
+        // if (condition1) {
+        //     System.out.println("condition1 is true");
+        // }
+        // else {
+        //     System.out.println("condition1 is not true");
+        // }
+        // // Prints: condition1 is not true
+
+
+
+        // else if Statements
+        // int testScore = 76;
+        // char grade;
+
+        // if (testScore >= 90) {
+        //     grade = 'A';
+        // } else if (testScore >= 80) {
+        //     grade = 'B';
+        // } else if (testScore >= 70) {
+        //     grade = 'C';
+        // } else if (testScore >= 60) {
+        //     grade = 'D';
+        // } else {
+        //     grade = 'F';
+        // }
+
+        // System.out.println("Grade: " + grade);
+
+
+
+        // Nested Conditional Statements
+        // boolean studied = true;
+        // boolean wellRested = true;
+
+        // if (wellRested) {
+        //     System.out.println("Best of luck today!");
+        //     if (studied) {
+        //         System.out.println("You are prepared for exam!");
+        //     } else {
+        //         System.out.println("Study before your exam!");
+        //     }
+        // } 
+
+
+        // NOT operator
+        boolean a = true;
+        System.out.println(!a);
+        System.out.println(!false); 
+
+        // AND operator
+        System.out.println("true && true: " + (true && true));
+        System.out.println("true && false: " + (true && false));
+        System.out.println("false && true: " + (false && true));
+        System.out.println("false && false: " + (false && false));
+
+        // OR operator
+        System.out.println("true || true: " + (true || true));
+        System.out.println("true || false: " + (true || false));
+        System.out.println("false && true: " + (false || true));
+        System.out.println("false && false: " + (false || false));
+
     }
 }
