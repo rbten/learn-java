2bbf610ba8c1476f92f9c696aef7270ced477221
thinh <thinh.personal@gmail.com>
2024-02-27 20:48:21 +0700

    Add cheat-sheet 9-inheritance-and-polymorphism

---
+++ codecademy-learn-java/9-inheritance-and-polymorphism/cheat-sheet/Animal.java
@@ -0,0 +1,11 @@
+// Parent Class
+class Animal {
+  // Animal class members
+}
+
+// Child Class
+class Dog extends Animal {
+  // Dog inherits traits from Animal 
+  
+  // additional Dog class members
+}
