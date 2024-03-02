170d2bada95fb91820f956c263355f8fcb48d809
thinh <thinh.personal@gmail.com>
2024-03-02 22:04:25 +0700

    Update cheat-sheet in 9-inheritance-and-polymorphism/

--- codecademy-learn-java/9-inheritance-and-polymorphism/cheat-sheet/Animal.java
+++ codecademy-learn-java/9-inheritance-and-polymorphism/cheat-sheet/Animal.java
@@ -1,19 +1,38 @@
 // Parent class
 class Animal {
   String sound;
-  Animal(String snd) {
+
+  public Animal() {
+
+  }
+
+  public Animal(String snd) {
     this.sound = snd;
+  }
+
+  public void greeting() {
+    System.out.println("The animal greets you.");
+  }
+
+  public static void main(String[] args) {
+    Animal animal1 = new Animal();
+    Animal cat1 = new Cat();
+    animal1.greeting();
+    cat1.greeting();
   }
 }
 
 // Child class
 class Dog extends Animal { 
   // super() method can act like the parent constructor inside the child class constructor.
-  Dog() {
+  public Dog() {
     super("woof");
   } 
-  // alternatively, we can override the constructor completely by defining a new constructor.
-  Dog() {
-    this.sound = "woof";
+}
+
+class Cat extends Animal {
+  public void greeting() {
+    System.out.println("The cat meows.");
   }
 }
+
