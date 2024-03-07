152982ee55dd42a8ce1b0edfc414de1412a0207d
thinh <thinh.personal@gmail.com>
2024-03-07 21:46:56 +0700

    Update cheat-sheet 11-two-dimensional-arrays/

--- codecademy-learn-java/11-two-dimensional-arrays/cheat-sheet/CheatSheet.java
+++ codecademy-learn-java/11-two-dimensional-arrays/cheat-sheet/CheatSheet.java
@@ -7,5 +7,16 @@ public class CheatSheet {
                 System.out.println("\tThe inner index is: " + inner);
             }
         }
+
+        int[][] twoDIntArray;
+        String[][] twoDStringArray;
+        int[][] twoDDoubleArray;
+
+        int [][] arr = {{1, 2, 3},
+                        {4, 5, 6}};
+
+        int retrieved = arr[1][0];
+
+        System.out.println(retrieved);
     }
 }
