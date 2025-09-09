26aeae8518b7739e3fa121a5d38458ef128b3793
thinh <thinh.personal@gmail.com>
2025-09-09 07:56:13 +0700

    update 4-project-attempt-2

---
+++ educative/learn-java/3-java-methods-and-data-types/4-project-attempt-2/MyJavaApp.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+ import java.util.Random;
+
+ public class MyJavaApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+
+        // Adding 1 to ensure range 1-10
+        // random.nextInt(10) generates number from 0-9
+        // int randomNumber = random.nextInt(10) + 1;
+
+        // System.out.println(randomNumber);
+
+        int num1;
+        int num2;
+        String userName;
+        String op;
+        String question;
+        String welcomeMessage;
+        String farewellMessage;
+
+
+
+    }
+ }
