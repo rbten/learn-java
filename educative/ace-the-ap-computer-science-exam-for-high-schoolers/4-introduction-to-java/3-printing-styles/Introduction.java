dc1118d62b0b6a975ab57733c4bc4e66ddbaa004
thinh <thinh.personal@gmail.com>
2025-12-15 09:12:08 +0700

    3-printing-styles

--- educative/ace-the-ap-computer-science-exam-for-high-schoolers/4-introduction-to-java/3-printing-styles/Introduction.java
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/4-introduction-to-java/3-printing-styles/Introduction.java
@@ -2,5 +2,25 @@ class Introduction {
     public static void main(String[] args) {
         System.out.println("My Name is Robert");
         System.out.println("I am learning Java");
+
+        System.out.println();
+
+        String name = "Tracy";
+        int age = 25;
+        System.out.format("My name is %s and I am %d years old.%nI am learning Java.", name, age);
+
+        System.out.println("");
+        System.out.println("");
+
+        String nameOne = "John";
+        int ageOne = 35;
+        System.out.format("My name is %s and I am %d years old.%nI am learning JavaScript.", nameOne, ageOne);
+
+        System.out.println("");
+        System.out.println("");
+        
+        String nameTwo = "Mike";
+        int ageTwo = 32;
+        System.out.format("I am %s %nMy age is %d %nI like %f %nI am 99%% of energy", nameTwo, ageTwo, 3.14);
     }
 }
