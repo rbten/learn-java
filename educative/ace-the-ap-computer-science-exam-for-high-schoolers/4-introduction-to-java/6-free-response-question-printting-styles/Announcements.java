46080bc5e8af8f5ade371cab870217656e45cfd6
thinh <thinh.personal@gmail.com>
2025-12-29 20:50:56 +0700

    Solve question printing styles

---
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/4-introduction-to-java/6-free-response-question-printting-styles/Announcements.java
@@ -0,0 +1,17 @@
+class Announcements {
+    public static void main(String[] args) {
+        double timeTaken = 9.72;
+        String month = "May";
+        int year = 2008;
+
+        System.out.println("Usain Bolt has broken the world record for the men's 100 meters.");
+
+        System.out.format("Time taken: %.2f", timeTaken);
+        System.out.println();
+
+        System.out.format("Month: %s", month);
+        System.out.println();
+
+        System.out.format("Year : %d", year);
+    }
+}
