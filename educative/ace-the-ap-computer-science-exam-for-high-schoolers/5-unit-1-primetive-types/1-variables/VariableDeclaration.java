446a2fe096e4f17de60634415a16d58785e3f648
thinh <thinh.personal@gmail.com>
2026-01-03 09:13:05 +0700

    variables

---
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/1-variables/VariableDeclaration.java
@@ -0,0 +1,16 @@
+class VariableDeclaration {
+    public static void main(String[] args) {
+        int integerVariable = 7;
+        System.out.println(integerVariable);
+
+        double decimalVariable = 3.14;
+        System.out.println(decimalVariable);
+
+        boolean booleanVariable = true;
+        System.out.println(booleanVariable);
+
+        String textLiteralVariable = "Hello World!";
+        System.out.println(textLiteralVariable);
+
+    }
+}
