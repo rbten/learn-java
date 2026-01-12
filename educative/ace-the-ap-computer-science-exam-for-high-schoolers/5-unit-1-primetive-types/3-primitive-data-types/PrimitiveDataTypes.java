9679f831b4a75272512a149e3bd04f2c98000ff8
thinh <thinh.personal@gmail.com>
2026-01-12 21:02:00 +0700

    primitive data types

---
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/3-primitive-data-types/PrimitiveDataTypes.java
@@ -0,0 +1,8 @@
+class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        int integerNumber;
+        integerNumber = 7;
+
+        System.out.println(integerNumber);
+    }   
+}
