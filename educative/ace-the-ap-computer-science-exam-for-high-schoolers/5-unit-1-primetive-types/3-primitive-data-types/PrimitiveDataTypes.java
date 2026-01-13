deb382ee45b219bf8967841c16b524444c1dd2b0
thinh <thinh.personal@gmail.com>
2026-01-13 21:01:13 +0700

    primitive data types

--- educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/3-primitive-data-types/PrimitiveDataTypes.java
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/3-primitive-data-types/PrimitiveDataTypes.java
@@ -4,5 +4,23 @@ class PrimitiveDataTypes {
         integerNumber = 7;
 
         System.out.println(integerNumber);
+
+        double pi;
+        double avogadroNumber;
+
+        pi = 3.14159265359;
+        System.out.println(pi);
+
+        avogadroNumber =  6.02E23;
+        System.out.println(avogadroNumber);
+
+
+        boolean boolVariable;
+        boolVariable = true;
+        System.out.println(boolVariable);
+
+        boolVariable = false;
+        System.out.println(boolVariable);
     }   
+
 }
