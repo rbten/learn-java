f44c54859a518d39600480136f1fb181f7a8d1b4
thinh <thinh.personal@gmail.com>
2026-02-04 09:09:26 +0700

    Arithmetic Operators

---
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/5-arithmetic-operators/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+class ArithmeticOperators {
+    public static void main(String[] args) {
+        int var1 = 9;
+        int var2 = 2;
+
+        System.out.println(var1 + var2);
+        System.out.println(var1 - var2);
+        System.out.println(var1 * var2);
+        System.out.println(var1 / var2);
+        System.out.println(var1 % var2);
+    }
+}
