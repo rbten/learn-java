26b8a43d39f21de6283767dbb7ed997d69ed15ba
thinh <thinh.personal@gmail.com>
2026-04-04 09:03:33 +0700

    arithmetic-operators

--- educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/5-arithmetic-operators/ArithmeticOperators.java
+++ educative/ace-the-ap-computer-science-exam-for-high-schoolers/5-unit-1-primetive-types/5-arithmetic-operators/ArithmeticOperators.java
@@ -8,5 +8,19 @@ class ArithmeticOperators {
         System.out.println(var1 * var2);
         System.out.println(var1 / var2);
         System.out.println(var1 % var2);
+
+        // double type
+        double var3 = 9.5;
+        double var4 = 2.0;
+
+        System.out.println(var3 + var4);
+        System.out.println(var3 - var4);
+        System.out.println(var3 * var4);
+        System.out.println(var3 / var4);
+        System.out.println(var3 % var4);
+
+        // compound expressions
+        int result = 9 + (3 - 6) / 2 * 5 % 4;
+        System.out.println(result);
     }
 }
